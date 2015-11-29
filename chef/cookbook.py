169599cb21aa26a873e0f60443f5d37a1563a8b9
Brian Hodges <bhodges@zulily.com>
2015-11-28 22:31:20 -0800

    Moves url building to classmethod, adds trailing comment, fixes attributes mispelling

--- chef/cookbook.py
+++ chef/cookbook.py
@@ -2,16 +2,14 @@ from chef.base import ChefObject, ChefAPI
 from chef.exceptions import ChefServerNotFoundError
 
 class Cookbook(ChefObject):
-    """
-    A Chef Cookbook object.
-    """
+    """A Chef Cookbook object."""
 
     url = '/cookbooks'
 
     attributes = {
         'definitions': list,
         'name': str,
-        'attributetes': list,
+        'attributes': list,
         'files': list,
         'json_class': str,
         'providers': list,
@@ -23,22 +21,8 @@ class Cookbook(ChefObject):
         'version': str,
         'recipes': list,
         'root_files': list,
-        'chef_type': str
+        'chef_type': str,
     }
-
-
-    @staticmethod
-    def versions(name, api=None):
-        """
-        Get a list of versions for the named cookbook
-        """
-        api = api or ChefAPI.get_global()
-        url = "{0}/{1}".format(Cookbook.url, name)
-        try:
-            data = api[url]
-        except ChefServerNotFoundError:
-            return list()
-        return list([ rev['version'] for rev in data[name]['versions'] ])
 
 
     def __getitem__(self, attr):
